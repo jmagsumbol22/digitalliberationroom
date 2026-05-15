7d9d75b7b0f318822219ba9897eb7ab5190802ce
Jocelyn Magsumbol <jmagsumbol@gmail.com>
2026-05-14 23:19:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function showSection(sectionId) {
-  const sections = document.querySelectorAll(".card");
+  const cards = document.querySelectorAll(".card");
 
-  sections.forEach(function(section) {
-    section.classList.remove("active");
+  cards.forEach((card) => {
+    card.classList.remove("active");
   });
 
   document.getElementById(sectionId).classList.add("active");
+
   document.getElementById("message").textContent = "";
 }
 
 function wrongAnswer() {
   document.getElementById("message").textContent =
-    "The lock does not open. Try thinking beyond individual blame.";
+    "PLACEHOLDER ERROR MESSAGE";
 }
